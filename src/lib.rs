518634bf5941b349634e8f7ddd7f8cbcf2452a2a
maikklein <maikklein@googlemail.com>
2017-02-24 15:37:52 +0100

    use traits for achieve a better drop

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,57 @@
-//#![feature(test)]
-//extern crate test;
+#![feature(test)]
+#![feature(specialization)]
+extern crate test;
 use std::collections::HashMap;
 use std::any::TypeId;
+use std::mem::transmute;
 
-pub struct VecTypeStore<C: DropType> {
-    store: TypeStore,
-    _m: ::std::marker::PhantomData<C>,
-}
-
-impl<D: DropType> std::ops::Drop for VecTypeStore<D> {
-    fn drop(&mut self) {
-        for (&type_id, &ptr) in self.store.store.iter() {
-            D::drop_type(type_id, ptr);
+#[macro_export]
+macro_rules! ptr_as_ref{
+    ($ptr: expr) => {
+        {
+            let fat_ptr = std::mem::transmute::<*mut DynamicType, FatPointer>($ptr);
+            std::mem::transmute(fat_ptr.obj_ptr)
         }
     }
 }
 
-impl<D: DropType> VecTypeStore<D> {
-    pub fn types(&self) -> std::collections::hash_map::Keys<std::any::TypeId, *mut ()> {
-        self.store.types()
+//trait Type<C> {
+//    fn has_type() -> bool;
+//}
+//
+//struct Test;
+//
+//impl<T> Type<Test> for T {
+//    default fn has_type() -> bool {
+//        false
+//    }
+//}
+//
+//impl Type<Test> for i32 {
+//    fn has_type() -> bool {
+//        true
+//    }
+//}
+
+pub struct VecTypeStore {
+    store: TypeStore,
+}
+
+impl VecTypeStore {
+    pub fn type_len(&self) -> usize {
+        self.store.type_len()
     }
 
     pub fn new() -> Self {
-        VecTypeStore {
-            store: TypeStore::new(),
-            _m: ::std::marker::PhantomData,
-        }
+        VecTypeStore { store: TypeStore::new() }
     }
 
     pub fn contains_type<T: 'static>(&self) -> bool {
         self.store.contains_type::<Vec<T>>()
+    }
+
+    pub fn contains_type_id(&self, type_id: TypeId) -> bool {
+        self.store.contains_type_id(type_id)
     }
 
     pub fn insert<T: 'static>(&mut self, val: Vec<T>) {
@@ -40,6 +62,10 @@ impl<D: DropType> VecTypeStore<D> {
         self.store.get::<Vec<T>>()
     }
 
+    ////pub fn access_mut<T: 'static>(&mut self) -> Option<*mut Vec<T>> {
+    ////    self.store.access_mut::<Vec<T>>()
+    ////}
+
     pub fn get_mut<T: 'static>(&mut self) -> Option<&mut Vec<T>> {
         self.store.get_mut::<Vec<T>>()
     }
@@ -49,38 +75,59 @@ impl<D: DropType> VecTypeStore<D> {
     }
 }
 
-pub trait DropType {
-    fn drop_type(type_id: TypeId, ptr: *mut ());
+#[repr(C)]
+pub struct FatPointer {
+    pub obj_ptr: u64,
+    pub trait_ptr: u64,
 }
+
+pub trait DynamicType {}
+impl<T> DynamicType for T {}
 
 pub struct TypeStore {
-    store: HashMap<std::any::TypeId, *mut ()>,
+    store: HashMap<std::any::TypeId, *mut DynamicType>,
+}
+impl std::ops::Drop for TypeStore {
+    fn drop(&mut self) {
+        for &ptr in self.store.values().into_iter() {
+            unsafe {
+                Box::from_raw(ptr);
+            }
+        }
+    }
 }
 
-
 impl TypeStore {
-    pub fn types(&self) -> std::collections::hash_map::Keys<std::any::TypeId, *mut ()> {
-        self.store.keys()
+    pub fn type_len(&self) -> usize {
+        self.store.keys().len()
     }
 
     pub fn contains_type<T: 'static>(&self) -> bool {
-        self.store.contains_key(&std::any::TypeId::of::<T>())
+        self.contains_type_id(std::any::TypeId::of::<T>())
+    }
+
+    pub fn contains_type_id(&self, type_id: TypeId) -> bool {
+        self.store.contains_key(&type_id)
     }
 
     pub fn insert<T: 'static>(&mut self, val: T) {
-        let ptr = Box::into_raw(Box::new(val)) as *mut ();
+        let ptr = Box::into_raw(Box::new(val));
         let t = std::any::TypeId::of::<T>();
         self.store.insert(t, ptr);
     }
 
     pub fn get<T: 'static>(&self) -> Option<&T> {
         let t = std::any::TypeId::of::<T>();
-        self.store.get(&t).map(|&ptr| unsafe { std::mem::transmute(ptr) })
+        self.store.get(&t).map(|&ptr| unsafe {
+            let fat_ptr = std::mem::transmute::<*mut DynamicType, FatPointer>(ptr);
+            let f: &T = std::mem::transmute(fat_ptr.obj_ptr);
+            f
+        })
     }
 
     pub fn get_mut<T: 'static>(&mut self) -> Option<&mut T> {
         let t = std::any::TypeId::of::<T>();
-        self.store.get(&t).map(|&ptr| unsafe { std::mem::transmute(ptr) })
+        self.store.get(&t).map(|&ptr| unsafe { ptr_as_ref!(ptr) })
     }
 
     pub fn get_mut2<A: 'static, B: 'static>(&mut self) -> Option<(&mut A, &mut B)> {
@@ -91,82 +138,104 @@ impl TypeStore {
         if (r1.is_none() || r2.is_none()) {
             return None;
         }
-        Some((unsafe { std::mem::transmute(*r1.unwrap()) },
-              unsafe { std::mem::transmute(*r2.unwrap()) }))
+        Some((unsafe { ptr_as_ref!(*r1.unwrap()) }, unsafe { ptr_as_ref!(*r2.unwrap()) }))
     }
+
+    //pub fn get_mut3<A: 'static, B: 'static, C: 'static>(&mut self)
+    //                                                    -> Option<(&mut A, &mut B, &mut C)> {
+    //    let a = std::any::TypeId::of::<A>();
+    //    let b = std::any::TypeId::of::<B>();
+    //    let c = std::any::TypeId::of::<C>();
+    //    let r1 = self.store.get(&a);
+    //    let r2 = self.store.get(&b);
+    //    let r3 = self.store.get(&c);
+    //    if (r1.is_none() || r2.is_none()) {
+    //        return None;
+    //    }
+    //    Some((unsafe { ptr_as_mut_ref(*r1.unwrap()) },
+    //          unsafe { ptr_as_mut_ref(*r2.unwrap()) },
+    //          unsafe { ptr_as_mut_ref(*r3.unwrap()) }))
+    //}
 
     pub fn new() -> Self {
         TypeStore { store: HashMap::new() }
     }
 }
 
-pub struct Ecs<D: DropType> {
-    world: Vec<VecTypeStore<D>>,
+pub struct Ecs {
+    world: Vec<VecTypeStore>,
 }
-pub struct AddEntity<'r, Ecs: 'r, A> {
+pub struct AddEntity<'r, A> {
     ecs: &'r mut Ecs,
     index: usize,
-    _a: ::std::marker::PhantomData<A>,
+    _m: ::std::marker::PhantomData<A>,
 }
 
-impl<'r, D: DropType, A: 'static> AddEntity<'r, Ecs<D>, A> {
+impl<'r, A: 'static> AddEntity<'r, A> {
     pub fn add_entity(&mut self, a: A) {
         self.ecs.world[self.index].get_mut::<A>().unwrap().push(a);
     }
 }
-
-pub struct AddEntity2<'r, Ecs: 'r, A, B> {
+pub struct AddEntity2<'r, A, B> {
     ecs: &'r mut Ecs,
     index: usize,
-    _a: ::std::marker::PhantomData<A>,
-    _b: ::std::marker::PhantomData<B>,
+    _m: ::std::marker::PhantomData<(A, B)>,
 }
-impl<'r, D: DropType, A: 'static, B: 'static> AddEntity2<'r, Ecs<D>, A, B> {
+
+impl<'r, A: 'static, B: 'static> AddEntity2<'r, A, B> {
     pub fn add_entity2(&mut self, a: A, b: B) {
-        self.ecs.world[self.index].get_mut::<A>().unwrap().push(a);
-        self.ecs.world[self.index].get_mut::<B>().unwrap().push(b);
+        let (va, vb) = self.ecs.world[self.index].get_mut2::<A, B>().unwrap();
+        va.push(a);
+        vb.push(b);
     }
 }
-impl<D: DropType> Ecs<D> {
-    pub fn add_entity<'r, A: 'static>(&'r mut self) -> AddEntity<'r, Self, A> {
-        let p = self.world
-            .iter()
-            .position(|store| {
-                store.types().len() == 1 && store.types().all(|ty| *ty == TypeId::of::<A>())
-            });
-        let index = p.unwrap_or(self.world.len());
-        if p.is_none() {
-            let mut store = VecTypeStore::new();
-            store.insert(Vec::<A>::new());
-            self.world.push(store);
-        }
+
+impl Ecs {
+    pub fn add_entity<A: 'static>(&mut self) -> AddEntity<A> {
+        let index: usize = {
+            let p = {
+                self.world
+                    .iter()
+                    .position(|store| store.type_len() == 1 && store.contains_type::<A>())
+            };
+            let index = p.unwrap_or(self.world.len());
+            if p.is_none() {
+                let mut store = VecTypeStore::new();
+                store.insert(Vec::<A>::new());
+                self.world.push(store);
+            }
+            index
+        };
         AddEntity {
             ecs: self,
             index: index,
-            _a: ::std::marker::PhantomData,
+            _m: ::std::marker::PhantomData,
         }
     }
 
-    pub fn add_entity2<'r, A: 'static, B: 'static>(&'r mut self) -> AddEntity2<'r, Self, A, B> {
-        let p = self.world
-            .iter()
-            .position(|store| {
-                store.types().len() == 2 &&
-                store.types().all(|ty| *ty == TypeId::of::<A>() || *ty == TypeId::of::<B>())
-            });
-        let index = p.unwrap_or(self.world.len());
-        if p.is_none() {
-            let mut store = VecTypeStore::new();
-            store.insert(Vec::<A>::new());
-            store.insert(Vec::<B>::new());
-            self.world.push(store);
-        }
-
+    pub fn add_entity2<'r, A: 'static, B: 'static>(&mut self) -> AddEntity2<A, B> {
+        let index: usize = {
+            let p = {
+                self.world
+                    .iter()
+                    .position(|store| {
+                        store.type_len() == 2 &&
+                        (store.contains_type::<A>() || store.contains_type::<B>())
+                    })
+            };
+            let index = p.unwrap_or(self.world.len());
+            if p.is_none() {
+                let mut store = VecTypeStore::new();
+                store.insert(Vec::<A>::new());
+                store.insert(Vec::<B>::new());
+                self.world.push(store);
+            }
+            index
+        };
         AddEntity2 {
             ecs: self,
             index: index,
-            _a: ::std::marker::PhantomData,
-            _b: ::std::marker::PhantomData,
+            _m: ::std::marker::PhantomData,
         }
     }
 
@@ -187,7 +256,6 @@ impl<D: DropType> Ecs<D> {
     {
         for store in self.world.iter_mut() {
             if let Some((i1, i2)) = store.get_mut2::<A, B>() {
-                println!("len {} {}", i1.len(), i2.len());
                 for (a, b) in i1.iter_mut().zip(i2.iter_mut()) {
                     f(a, b);
                 }
@@ -195,79 +263,110 @@ impl<D: DropType> Ecs<D> {
         }
     }
 
-    pub fn new() -> Ecs<D> {
+    pub fn new() -> Ecs {
         Ecs { world: Vec::new() }
     }
 }
 
-pub fn drop_vec<T>(ptr: *mut ()) {
-    let tptr: *mut Vec<T> = unsafe { ::std::mem::transmute(ptr) };
-    unsafe { Box::from_raw(tptr) };
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
 
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//    use test::Bencher;
-//
-//    pub struct PosVelDrop;
-//    impl DropType for PosVelDrop {
-//        fn drop_type(type_id: TypeId, ptr: *mut ()) {
-//            if TypeId::of::<Vec<Position>>() == type_id {
-//                drop_vec::<Position>(ptr);
-//            }
-//            if TypeId::of::<Vec<Velocity>>() == type_id {
-//                drop_vec::<Velocity>(ptr);
-//            }
-//        }
-//    }
-//
-//    #[derive(Debug)]
-//    struct Position {
-//        x: f32,
-//        y: f32,
-//    }
-//
-//    #[derive(Debug, Copy, Clone)]
-//    struct Velocity {
-//        x: f32,
-//        y: f32,
-//    }
-//    static N_POS_VEL: usize = 1000;
-//    static N_POS: usize = 9000;
-//    pub fn build() -> Ecs<PosVelDrop> {
-//        let mut ecs = Ecs::new();
-//        {
-//            let mut entity_add_pv = ecs.add_entity2::<Position, Velocity>();
-//            for _ in (0..N_POS_VEL) {
-//                let pos = Position { x: 0.0, y: 0.0 };
-//                let vel = Velocity { x: 0.0, y: 0.0 };
-//                entity_add_pv.add_entity2(pos, vel);
-//            }
-//        }
-//        {
-//            let mut entity_add_p = ecs.add_entity::<Position>();
-//            for _ in (0..N_POS) {
-//                let pos = Position { x: 0.0, y: 0.0 };
-//                entity_add_p.add_entity(pos);
-//            }
-//        }
-//        ecs
-//    }
-//
-//    #[bench]
-//    fn bench_build(b: &mut Bencher) {
-//        b.iter(|| build());
-//    }
-//    #[bench]
-//    fn bench_update(b: &mut Bencher) {
-//        let mut ecs = build();
-//        b.iter(|| {
-//            ecs.update2(|p: &mut Position, v: &mut Velocity| {
-//                //println!("{:?}", p);
-//                p.x += v.x;
-//                p.y += v.y;
-//            });
-//        });
-//    }
-//}
+    #[derive(Debug)]
+    struct Position {
+        x: f32,
+        y: f32,
+    }
+
+    #[derive(Debug, Copy, Clone)]
+    struct Velocity {
+        x: f32,
+        y: f32,
+    }
+    static N_POS_VEL: usize = 1000;
+    static N_POS: usize = 9000;
+    pub fn build() -> Ecs {
+        let mut ecs = Ecs::new();
+        {
+            let mut entity_add_pv = ecs.add_entity2::<Position, Velocity>();
+            for i in (0..N_POS_VEL) {
+                let pos = Position {
+                    x: 0.0,
+                    y: i as f32,
+                };
+                let vel = Velocity { x: 1.0, y: 0.0 };
+                entity_add_pv.add_entity2(pos, vel);
+            }
+        }
+        {
+            let mut entity_add_p = ecs.add_entity::<Position>();
+            for i in (0..N_POS) {
+                let pos = Position {
+                    x: 9999.0 as f32,
+                    y: 0.0,
+                };
+                entity_add_p.add_entity(pos);
+            }
+        }
+        ecs
+    }
+
+
+    #[test]
+    fn t1() {
+        let mut t = TypeStore::new();
+        //t.insert(Vec::<u32>::new());
+        //{
+        //    let v = {
+        //        t.get_mut::<Vec<u32>>().unwrap().iter_mut()
+        //    };
+        //}
+    }
+    #[test]
+    fn t() {
+        //    let mut v = VecTypeStore::<PosVelDrop>::new();
+        //    v.insert(Vec::<Position>::new());
+        //    v.insert(Vec::<Velocity>::new());
+        //    let mut a = v.access_mut::<Position>();
+        //    let mut b = v.access_mut::<Velocity>();
+        //    let i = a.iter_mut();
+        //    let i2 = b.iter_mut();
+    }
+    //#[test]
+    //fn test_type(){
+    //    println!("{}", <i32 as Type<Test>>::has_type());
+    //    println!("{}", <String as Type<Test>>::has_type());
+    //}
+    //#[test]
+    //fn test_update() {
+    //    let mut ecs = build();
+    //    for _ in 0..10 {
+    //        ecs.update2(|p: &mut Position, v: &mut Velocity| {
+    //            p.x += v.x;
+    //            p.y += v.y;
+    //            //println!("{:?}", p);
+    //        });
+    //    }
+    //    ecs.update(|p: &mut Position| {
+    //        //println!("{:?}", p);
+    //    });
+    //}
+    #[bench]
+    fn bench_build(b: &mut Bencher) {
+        b.iter(|| build());
+    }
+    #[bench]
+    fn bench_update(b: &mut Bencher) {
+        let mut ecs = build();
+        b.iter(|| {
+            ecs.update2(|p: &mut Position, v: &mut Velocity| {
+                p.x += v.x;
+                p.y += v.y;
+            });
+            ecs.update(|p: &mut Position| {
+                //println!("{:?}", p);
+            });
+        });
+    }
+}
